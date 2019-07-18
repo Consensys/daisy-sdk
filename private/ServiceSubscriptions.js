bf379b0c2f61120417d40bde7063e58a733aa208
Patricio López Juri <lopezjuripatricio@gmail.com>
2019-07-18 12:11:29 -0400

    Allow creating invitation from SDK (#3)
    * Allow creating invitation from SDK
    
    * Improve createInvitation

--- private/ServiceSubscriptions.js
+++ private/ServiceSubscriptions.js
@@ -33,6 +33,42 @@ class ServiceSubscriptions extends SubscriptionProductClient {
       });
     });
   }
+
+  /**
+   * Create an invitation link
+   * @async
+   * @param {Object|string} plan - Plan object or plan.id string.
+   * @param {Object} [params={}] - Invitation attributes.
+   * @param {Boolean} [params.active=true] - Is public available or not.
+   * @param {Number} [params.maxUsages=0] - Invitation max allowed usages. Set to `0` for unlimited.
+   * @param {Object} [params.callbackExtra={}] - Extra payload, helpful to identify the user.
+   * @param {String} [params.callbackURL] - Callback URL to receive Daisy ID from the subscription
+   * @param {String} [params.redirectURLDefault] - On success default redirect button.
+   * @returns {Promise<Invitation>} - Invitation object with public link and identifier.
+   */
+  createInvitation(
+    plan,
+    params = { active: true, maxUsages: 0, callbackExtra: {} }
+  ) {
+    if (!plan) {
+      throw new Error("Missing first argument: plan");
+    }
+
+    const data = {
+      maxUsages: Number(params["maxUsages"]),
+      active: params["active"],
+      callbackExtra: JSON.parse(JSON.stringify(params["callbackExtra"])), // TODO: check if plan JS Object.
+      callbackURL: params["callbackURL"],
+      redirectURLDefault: params["redirectURLDefault"],
+    };
+    const planId = plan["id"] || plan;
+
+    return this.request({
+      method: "post",
+      url: `/plans/${planId}/invitations/`,
+      data,
+    }).then(({ data: body }) => body.data);
+  }
 }
 
 module.exports = ServiceSubscriptions;
