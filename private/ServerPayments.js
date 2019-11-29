c91b4bc934955253c86da42c48f0ee4fe097f139
Patricio Lopez J <lopezjuripatricio@gmail.com>
2019-11-29 09:57:37 -0300

    Allow tokenAddress and walletAddress when creating Invoice

--- private/ServerPayments.js
+++ private/ServerPayments.js
@@ -10,11 +10,19 @@ class ServerPayments extends ClientPayments {
   }
 
   createInvoice(params = {}) {
+    if (!params || !params.invoicedPrice) {
+      throw new TypeError(`Missing params.invoicedPrice argument.`);
+    }
+
+    // TODO: maybe add check if user forgot to add the decimals to the price.
+
     const data = {
-      invoicedPrice: params.invoicedPrice,
+      invoicedPrice: params.invoicedPrice, // required
       invoicedEmail: params.invoicedEmail,
       invoicedName: params.invoicedName,
       invoicedDetail: params.invoicedDetail,
+      tokenAddress: params.tokenAddress,
+      walletAddress: params.walletAddress,
     };
 
     return this.request({
